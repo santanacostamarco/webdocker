ce6cd7aece39dfa9552884f27705d683ae795db5
Marco Santana <marco.santana@menu.com.vc>
2019-10-26 22:24:47 -0300

    setup mysql

--- app/index.php
+++ app/index.php
@@ -1,1 +1,11 @@
-<?php echo phpinfo() ?>
+<?php
+
+    $conn = mysqli_connect("mysql", $_ENV["MYSQL_USER"], $_ENV["MYSQL_PASS"], $_ENV["MYSQL_DB_NAME"]);
+
+    if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    echo phpinfo();
+    
+?>
